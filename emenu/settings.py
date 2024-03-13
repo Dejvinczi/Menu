a2f2a4a1119a59dbe4cf36b599bc3ad48cfa35b7
dejvinczi <dejvinczi.main@gmail.com>
2024-03-13 17:35:03 +0100

    Refactor structure

--- emenu/settings.py
+++ emenu/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'emenu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myproject',
+        'USER': 'myproject_user',
+        'PASSWORD': 'myproject_database_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
