6fd569ad668d788b3f77679eaf350dc55a8ef7f9
dejvinczi <dejvinczi.main@gmail.com>
2024-03-13 21:47:39 +0100

    Cover with tests user/test_models.py

--- user/tests/test_models.py
+++ user/tests/test_models.py
@@ -20,3 +20,33 @@ class TestUser:
 
         assert user.email == user_data["email"]
         assert user.check_password(user_data["password"])
+
+    def test_create_user_without_email_error(self):
+        user_data = {
+            "email": "",
+            "password": "testuser123",
+        }
+        with pytest.raises(ValueError) as e_info:
+            user = get_user_model().objects.create_user(**user_data)
+
+        assert str(e_info.value) == "User must have an email address."
+
+    def test_create_superuser_with_email_successful(self):
+        user_data = {
+            "email": "testsuperuser@example.com",
+            "password": "testsuperuser123",
+        }
+        user = get_user_model().objects.create_superuser(**user_data)
+
+        assert user.email == user_data["email"]
+        assert user.check_password(user_data["password"])
+
+    def test_create_superuser_without_email_error(self):
+        user_data = {
+            "email": "",
+            "password": "testsuperuser123",
+        }
+        with pytest.raises(ValueError) as e_info:
+            user = get_user_model().objects.create_superuser(**user_data)
+
+        assert str(e_info.value) == "User must have an email address."
